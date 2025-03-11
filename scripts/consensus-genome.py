db679258e40b75ddf1cc27bcac1b27dccef3166d
Matthew Jobin <matt.jobin@ucsf.edu>
2025-03-11 06:30:20 -0700

    Log level chosen by CLI log-level, or if blank, reads from config, or if no config, defaults to INFO.
    Adds run_pipeline invocation with fixed "consensus-genome" snakefile.
    
    Commented out output stub until output structure can be nailed down.

--- scripts/consensus-genome.py
+++ scripts/consensus-genome.py
@@ -9,6 +9,8 @@ import subprocess
 from pathlib import Path
 from src.logging_utils import setup_logger
 from src.config_utils import setup_config
+from src.pipeline_utils import run_pipeline, parse_arguments
+
 
 # -------------------------
 # Definitions
@@ -17,34 +19,37 @@ from src.config_utils import setup_config
 LOG_FILENAME = "logs/consensus-genome.log"
 PIPELINE_NAME = "consensus-genome"
 
+
 # -------------------------
 # Setup
 # -------------------------
 
-
 PROJECT_ROOT = Path(__file__).resolve().parent.parent  # Two levels up from scripts/
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--log-level", default="DEBUG", choices=["DEBUG", "INFO", "WARNING", "ERROR"])
-parser.add_argument("-p", "--pipeline", help="Workflow pipeline file to run (e.g., 'consensus-genome') Defaults to the main pipeline.", default=None)
-parser.add_argument("-c", "--config_file", default=None, choices=["local", "cluster", "cluster_submit"])
-parser.add_argument("--dry-run", action="store_true", help="Perform a dry run")
-
-args = parser.parse_args()
-level = getattr(logging, args.log_level.upper())
+args = parse_arguments()
 
 config = setup_config(PROJECT_ROOT, args.config_file)
-logger = setup_logger("consensus-genome", LOG_FILENAME, level=snakemake.config.get("logging", {}).get("level", "INFO"))
-logger.info(f"Starting consensus genomes with input: {snakemake.input[0]}")
+if args.log_level is None:
+    log_level = getattr(logging, config.get("logging", {}).get("level", "INFO").upper())
+else:
+    log_level = getattr(logging, args.log_level.upper())
+logger = setup_logger("cypherid", LOG_FILENAME, level=log_level)
+
+logger.info(f"Starting consensus-genome pipeline.")
+
+
+# -------------------------
+# Pipeline
+# -------------------------
 
 snakefile =  PROJECT_ROOT / "workflows" / PIPELINE_NAME / "Snakefile"
 if not snakefile.exists():
     print(f"Error: Snakefile {snakefile} not found.")
     sys.exit(1)
 
+run_pipeline(logger=logger, project_root=PROJECT_ROOT, pipeline_name=PIPELINE_NAME, dry_run=args.dry_run)
 
+# with open(snakemake.output[0], "w") as f:
+#     f.write("Done")
 
-with open(snakemake.output[0], "w") as f:
-    f.write("Done")
-
-logger.info(f"Consensus genome run completed.")
+logger.info(f"Consensus-genome run completed.")
