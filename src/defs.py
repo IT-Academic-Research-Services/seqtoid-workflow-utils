0142385a67c90cd342eb6f1700d995dca3f39a5b
Matthew Jobin <matt.jobin@ucsf.edu>
2025-03-12 12:35:22 -0700

    Initial import with fastq, fasta, and tag defs.

---
+++ src/defs.py
@@ -0,0 +1,34 @@
+"""
+Common definitions.
+"""
+
+
+# ---------------
+# Character definitions
+# ---------------
+
+
+# ---------------
+# File definitions
+# ---------------
+
+# ---------------
+# Bio File definitions
+# ---------------
+
+# Canonical for creating files
+FASTA_EXT = '.fasta'
+FASTQ_EXT = '.fastq'
+R1_TAG = 'R1'
+R2_TAG = 'R2'
+
+# Sets for detecting file types
+FASTA_EXT_SET = {'.fa', '.fasta', '.fa.gz', '.fasta.gz'}
+FASTQ_EXT_SET = {'.fq', '.fastq', '.fastq.gz', '.fq.gz'}
+R1_TAG_SET = {'R1', 'r1'}
+R2_TAG_SET = {'R2', 'r2'}
+
+# ---------------
+# Bio definitions
+# ---------------
+
