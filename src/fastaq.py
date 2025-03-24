7bcad354b6c867b2fc67e81f310c768996b34649
Matthew Jobin <matt.jobin@ucsf.edu>
2025-03-24 15:45:34 -0700

    Added fasta_iterate.

--- src/fastaq.py
+++ src/fastaq.py
@@ -50,3 +50,24 @@ def fastq_iterate(file_handle):
             return None
 
         yield header, seq, qual
+
+def fasta_iterate(file_handle):
+    """
+    Iterate over a FASTA file.
+    :param file_handle: File handle.
+    :return: Generator or None.
+    """
+    while True:
+        header = file_handle.readline().strip()
+        if header == '':
+            break
+        if not header.startswith('>'):
+            get_logger().error(f"header is {header} but does not start with '>'")
+            return None
+
+        seq = file_handle.readline().strip()
+        if seq == '':
+            get_logger().error(f"header is {header} but no sequence follows")
+            return None
+
+        yield header, seq
